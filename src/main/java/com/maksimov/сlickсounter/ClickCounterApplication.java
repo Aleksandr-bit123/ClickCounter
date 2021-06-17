98a2898dbb2e70f135ae9b1fe46b2065b8be87b1
vavomiskam <vavomiskam@yandex.ru>
2021-06-17 13:49:01 +0300

    Initial commit

---
+++ src/main/java/com/maksimov/сlickсounter/ClickCounterApplication.java
@@ -0,0 +1,14 @@
+package com.maksimov.сlickсounter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ClickCounterApplication {
+
+	public static void main(String[] args) {
+		run(ClickCounterApplication.class, args);
+	}
+
+}
